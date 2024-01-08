1eb3b094e0d4c28b8b986ae0c0b8a5a6faf27f71
Akruti <akrutitaori@gmail.com>
2024-01-08 22:29:54 +0530

    Styling the signup page

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,5 +1,86 @@
 import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import BackgroundImage from "../components/BackgroundImage";
 
 export default function Signup(){
-  return <div>Signup</div>
+  return (<Container>
+    <BackgroundImage />
+    <div className="content">
+      <Header login/>
+      <div className="body flex column j-center a-center">
+        <div className="text flex column">
+          <h1>Unlimited movies, TV shows and more</h1>
+          <h4>Watch anywhere. Cancel anytime</h4>
+          <h6>Ready to watch? Enter your email to create or restart membership</h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder="Email Address" name="email"/>
+          <input type="password"  placeholder="Password" name="password"/>
+          <button>Get Started</button>
+        </div>
+        <button>Log In</button>
+      </div>
+    </div>
+  </Container>
+  );
 }
+
+const Container = styled.div`
+position: relative;
+.content{
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,,0.5);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 15vh 85vh;
+  .body{
+    gap: 1rem;
+    .text{
+      gap: 1rem;
+      text-align: center;
+      font-size: 2rem;
+      h1 {
+        padding: 0 25rem;
+      }
+    }
+    .form{
+      display: grid;
+      // grid-template-columns:;
+      width: 60%;
+      input {
+        color:black;
+        border: none;
+        padding: 1.5rem;
+        font-size: 1.2rem;
+        border: 1px solid black;
+        &:focus {
+          outline: none;
+        }
+      }
+      button{
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        font-weight: bolder;
+        fontsize: 1.05rem;
+      }
+    }
+    button{
+      padding: 0.5rem 1rem;
+      background-color: #e50914;
+      border: none;
+      cursor: pointer;
+      color: white;
+      border-radius: 0.2rem;
+      font-weight: bolder;
+      fontsize: 1.05rem;
+    }
+  }
+}
+`;
