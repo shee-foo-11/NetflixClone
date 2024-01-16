4e3f7ba8bdb5840f8d950788daa74008e510b17a
Akruti <akrutitaori@gmail.com>
2024-01-16 16:03:55 +0530

    Added functionality to fetch movies from TMDB API

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react"; 
+import React, { useEffect, useState} from "react"; 
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import backgrounfImage from "../assets/home.jpg";
@@ -6,15 +6,31 @@ import MovieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getGenres } from "../store";
+import { fetchMovies } from "../store";
 
 export default function Netflix(){
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
+  const genresLoaded = useSelector((state)=> state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies);
+  const dispatch = useDispatch();
+
+  useEffect(()=> {
+    dispatch(getGenres());
+  },[]);
+
+  useEffect(()=>{
+    if(genresLoaded) dispatch(fetchMovies({type:"all"}))
+  });
 
   window.onscroll = ()=> {
     setIsScrolled(window.pageYOffset===0 ? false:true);
     return () => (window.onscroll = null);
   };
+
+  
   
   return (<Container>
     <Navbar isScrolled = {isScrolled}/>
